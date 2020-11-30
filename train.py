fa727241fd925d4e7497b1efa18fd318f2d39766
BOUHAFSADMI <sadmibouhafs@gmail.com>
2020-11-30 23:23:57 +0100

    Implemented AutoML pipeline

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import numpy as np
-from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -56,14 +55,12 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.35)
 
 run = Run.get_context()
 
-    
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")    
 
     args = parser.parse_args()
 
